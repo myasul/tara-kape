438e76e5348c369ca79407376c4da24919a609d9
Matthew Yasul <yasulmatthew@gmail.com>
2022-05-09 15:42:14 +0800

    initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,12 @@
+export default function Index () {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            <div>
+                <button className="btn">Button daisyUI</button>
+            </div>
+        </>
+    )
+}
